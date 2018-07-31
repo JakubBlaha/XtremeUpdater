9642e3166c2d2a778151245120d5908e609a8213
JakubBlaha <jakub.blaha@gmail.com>
2018-07-31 10:40:23 +0200

    Updated launcher

--- utils/XtremeUpdater Launcher/main.py
+++ utils/XtremeUpdater Launcher/main.py
@@ -1,16 +1,18 @@
 import os
 from pygit2 import Repository, GIT_RESET_HARD
+from traceback import format_exc
 
 UP_TO_DATE = 'Already up to date.'
 
-repo = Repository(os.path.dirname(os.getcwd()))
+repo = Repository(os.getcwd() + '\\repo')
 try:
+    repo.remotes['origin'].fetch()
     repo.reset(repo.lookup_reference('refs/remotes/origin/master').get_object().oid, GIT_RESET_HARD)
 except:
-    pass
+    print(format_exc())
 
 try:
-    os.startfile('XtremeUpdater.exe')
+    os.startfile(os.path.abspath('repo/XtremeUpdater/XtremeUpdater.exe'))
 except OSError:
-    pass
+    print(format_exc())
     
